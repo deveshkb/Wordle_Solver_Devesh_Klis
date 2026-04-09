1d367972d0af763a6668852d0b190d5947f1d6e1
DeveshKB <dkb020102@gmail.com>
2026-04-09 19:47:38 +0400

    Main code v0.3

--- src/main.c
+++ src/main.c
@@ -4,11 +4,14 @@
 #include "../include/filtres.h"
 
 #define MAX_WORD_LENGTH 100
+#define MAX_LETTERS 10
 
 int main() {
     FILE *file;
     char word[MAX_WORD_LENGTH];
-    char lettre;
+
+    char incluses[MAX_LETTERS];
+    char exclues[MAX_LETTERS];
 
     file = fopen("ressource/bdd_wordle.txt", "r");
     
@@ -17,13 +20,35 @@ int main() {
         return 1;
     }
 
-    printf("Entrer une lettre a filtrer : ");
-    scanf(" %c", &lettre);
+    printf("Entrer les lettres a INCLURE (ex: ae) : ");
+    scanf("%s", incluses);
 
-    printf("\nMots contenant '%c' :\n", lettre);
+    printf("Entrer les lettres a EXCLURE (ex: zx) : ");
+    scanf("%s", exclues);
+
+    printf("\nMots filtres :\n");
 
     while (fscanf(file, "%s", word) != EOF) {
-        if (contient_lettre(word, lettre)) {
+
+        int valide = 1;
+
+        // Vérifier les lettres incluses
+        for (int i = 0; incluses[i] != '\0'; i++) {
+            if (!contient_lettre(word, incluses[i])) {
+                valide = 0;
+                break;
+            }
+        }
+
+        // Vérifier les lettres exclues
+        for (int i = 0; exclues[i] != '\0'; i++) {
+            if (!exclut_lettre(word, exclues[i])) {
+                valide = 0;
+                break;
+            }
+        }
+
+        if (valide) {
             printf("%s\n", word);
         }
     }
